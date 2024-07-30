a5253088ca7c3ec0f5e24259de4ebe58fa7c957b
Salah <saldevfree@Lubnas-MacBook-Air.local>
2024-07-30 19:02:24 +0300

    3 types of image processing

---
+++ shred_image.py
@@ -0,0 +1,35 @@
+# import matplotlib.pyplot as plt
+#
+# import blur_image
+# import mask_image
+# import shuftle_image
+# import numpy as np
+class ShredImage:
+    def run_func(self, hardness) -> str:
+        """Load in the file for extracting text."""
+        pass
+    '''TODO
+    def importing_image(path:str):
+        try:
+        "importing ann opening instruction"
+        except :
+    '''
+
+    # def simplify_func(self, full_file_name: str) -> dict:
+    #     """Extract text from the currently loaded file."""
+    #     pass
+
+# if __name__ == '__main__':
+#     test_path = "python_img.png"
+#     shuffle_image_obj = shuftle_image.ShuffleImage(test_path)
+#     shuffle_image_test = shuffle_image_obj.run_func("medium")
+#     plt.imshow(shuffle_image_test)
+#     plt.show()
+#     blur_image_obj = blur_image.BlurImage(test_path)
+#     blur_image_test = blur_image_obj.run_func("medium")
+#     plt.imshow(blur_image_test)
+#     plt.show()
+#     mask_image_obj = mask_image.MaskImage(test_path)
+#     mask_image_test = mask_image_obj.run_func("medium")
+#     plt.imshow(mask_image_test)
+#     plt.show()
