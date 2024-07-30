a6cdbb6a68c0ecc28e6ab65f66fe848a4755db8a
Ofir Bura <ofir.bura@grunitch.com>
2024-07-30 17:12:36 +0300

    changes to the bot.py file to add functionality of commands. added new dependency for pillow. refs #2

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
 import logging
+from pathlib import Path
+from io import BytesIO
+import random
+
+from PIL import Image
+import PIL
 
 import telebot
 
@@ -14,26 +20,84 @@ logger = logging.getLogger(__name__)
 bot = telebot.TeleBot(bot_secrets.BOT_TOKEN)
 
 
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message: telebot.types.Message):
+@bot.message_handler(commands=['help'])
+def display_help(message: telebot.types.Message):
     chat_id = message.chat.id
     username = message.from_user.username
     logger.info(f"> Start chat #{chat_id}. username: {username}")
-    bot.send_message(chat_id, "❤️ Hello! ❤️")
+    response = """
+This is a guess the image game.
+The bot will send an image and the users will have to guess what's in the image.
+Commands:
+for instructions type /help
+To start the game type /play
+Commands during game:
+To submit a guess type /guess <your_answer>
+To request a hint type /hint 
+"""
+    bot.send_message(chat_id, response)
 
 
-# Handle all other messages with content_type 'text'
+@bot.message_handler(commands=['play'])
+def start_game(message: telebot.types.Message):
+    chat_id = message.chat.id
+
+    files = list(Path("pictures/").glob("*.jpg"))
+    logger.info(f"test: {files}")
+
+    im: PIL.Image = Image.open(random.choice(files))
+    im2 = im.resize((200, 200))
+    im2 = im2.rotate(90)
+    bio = BytesIO()
+    bio.name = 'image.jpeg'
+    logger.info(f"name of image: {im.filename}")
+    im2.save(bio, 'JPEG')
+    bio.seek(0)
+    bot.send_photo(chat_id, photo=bio)
+
+    bot.send_photo(chat_id, photo=open('kitty.jpg', 'rb'))
+
+
+
+@bot.message_handler(commands=['guess'])
+def check_guess(message: telebot.types.Message):
+    chat_id = message.chat.id
+    guess = message.text
+    guesser_id = message.from_user.id
+    guesser_first_name = message.from_user.first_name
+
+    # player = get_player(guesser_id)
+    # image = session(chat_id).get_image()
+    # if image.check_guess(guess):
+    #     logger.info(f"the guess is correct")
+    #     # update score for this player, advertise correct and end the game session
+    # else:
+    #     logger.info(f"the guess is incorrect")
+    #     # no updates, once time is up then goes to next hint
+
+    logger.info(f"the guess is: {guess}, it was made by: {guesser_first_name}")
+
+    logger.info(message.from_user)
+    bot.send_message(chat_id, f"i dont know whether its correct or not, thank you {guesser_first_name}")
+
+@bot.message_handler(commands=['hint'])
+def request_hint(message: telebot.types.Message):
+    chat_id = message.chat.id
+
+    game_session = True
+    if not game_session:
+        bot.send_message(chat_id, "there has to be an ongoing game to use this command")
+        return
+    bot.send_photo(chat_id, photo=open('kitty.jpg', 'rb'), caption="there is your hint")
+
+
 @bot.message_handler(func=lambda message: True)
-def echo_message(message: telebot.types.Message):
+def nothing(message: telebot.types.Message):
     chat_id = message.chat.id
     username = message.from_user.username
     text = message.text
 
     logger.info(f"= Got on chat #{chat_id}/{username!r}: {text!r}")
-    response = text.replace("7", "🌸")
-
-    # bot.reply_to(message, response)
-    bot.send_message(chat_id, response)
 
 
 logger.info("* Start polling...")
